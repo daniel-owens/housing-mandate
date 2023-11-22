648bb64dba4d394ce9a86a9cdf1202fb12137885
Daniel Owens <danielowens82@gmail.com>
2023-11-21 19:12:56 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function updateCountdown() {
+    const endDate = new Date("2031-12-31T23:59:59").getTime();
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+
+    if (timeLeft < 0) {
+        document.getElementById("countdown-timer").innerHTML = "Countdown expired!";
+    } else {
+        const years = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 365));
+        const months = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+        const days = Math.floor((timeLeft % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("years").innerText = years.toString().padStart(2, '0');
+        document.getElementById("months").innerText = months.toString().padStart(2, '0');
+        document.getElementById("days").innerText = days.toString().padStart(2, '0');
+        document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+    }
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
